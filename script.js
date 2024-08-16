b5eaa1ce8b3305a7edcc14c40a11ca2ee959b727
oksana-shpak <oksana.shpak@convertdigital.com.au>
2024-08-16 18:44:19 +1000

    refactor: Improve magnet layout and responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 import Magnet from './magnet.js';
 
-const magnetElements = [...document.querySelectorAll('.magnet')];
-const magnets = magnetElements.map(element => new Magnet(element));
+let size = 100;
+let magnets;
+
+size = Math.floor(Math.random() * 200 + 10);
+
+init();
 
 window.onmousemove = handleMagnetism;
+window.onresize = init;
+
+function init() {
+  const rows = Math.floor((innerHeight - 16) / size);
+  const cols = Math.floor((innerWidth - 16) / size);
+  const count = rows * cols;
+
+  const elements = Array.from({ length: count }, () => {
+    const element = document.createElement('div');
+
+    element.className = 'magnet';
+    return element;
+  });
+
+  document.body.style.setProperty('--size', `${size}px`);
+  document.body.style.setProperty('--col-count', cols);
+
+  document.body.replaceChildren(...elements);
+  magnets = elements.map(element => new Magnet(element));
+}
 
 function handleMagnetism(event) {
   const x = event.clientX;
